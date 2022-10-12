afe995f192b61ce3ec35b0a590cd818eb972b83e
hyemis <hyem.is.1220@gmail.com>
2022-10-12 15:21:49 +0900

    배열 연습문제1

---
+++ java_demo05/src/kr/co/exam/Exam01.java
@@ -0,0 +1,25 @@
+package kr.co.exam;
+
+public class Exam01 {
+
+	public static void main(String[] args) {
+		/*
+		 * 배열의 크기가 3인 정수 배열을 만들고 3부터 시작하는 
+		 * 3의 배수 값으로 초기화 하세요.
+		 */
+		
+		/*
+		 * 배열의 크기가 5인 정수 배열을 만들고 5부터 -1 씩 감소된
+		 * 값으로 초기화 후 출력하세요. 
+		 */
+		
+		int arr1[] = new int[3];
+		for(int i = 0 ; i < arr1.length; i++) {
+			
+		}
+	
+	
+	
+	}
+
+}
