7d9a8377836282056861cfd80581fd818f6b0e0f
hyemis <hyem.is.1220@gmail.com>
2022-10-12 15:22:09 +0900

    배열 연습문제3

---
+++ java_demo05/src/kr/co/exam/Exam03.java
@@ -0,0 +1,32 @@
+package kr.co.exam;
+
+import java.util.Scanner;
+
+public class Exam03 {
+
+	public static void main(String[] args) {
+		/*
+		 * 사용자 입력으로 2~5 사이의 정수 값을 입력 받아 
+		 * 입력 받은 정수값과 동일한 크기의 배열을 생성 하고,
+		 * 배열의 값을 초기화 하기 위해 다시 사용자 입력을 사용하여 
+		 * 1~10 사이의 값만을 입력 받아 초기화 하세요. 
+		 */
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("2~5 사이의 정수값을 입력해주세요.");
+		int input = sc.nextInt();
+		
+		int arr1[] = new int[input];
+		
+		System.out.println("초기화 하고자 하는 1~10 사이의 정수값을 입력하세요.");
+		int input2 = sc.nextInt();
+		
+		for(int i = 0; i < arr1.length; i++) {
+			arr1[i] = input2;
+			System.out.println(arr1[i]);
+		}
+		
+	}
+
+}
