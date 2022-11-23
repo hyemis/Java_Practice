6cee11424a34efc34bbef790e66666f9bce35726
hyemis <hyem.is.1220@gmail.com>
2022-11-23 14:19:34 +0900

    SEQUECE 객체

---
+++ oracle_sql05/Scripts/qeury01.sql
@@ -0,0 +1,54 @@
+/*
+ * SEQUENCE 객체 
+ * 		- 번호 발생기 객체 
+ * 		- 정수값을 순차적으로 생성하는 객체 
+ * 		- PRIMARY KEY 로 사용하는 번호(ID) 등에 사용하여 정수값을 생성하기 위한 
+ * 		 용도로 많이 사용
+*/
+CREATE SEQUENCE SEQ1
+				START WITH 10
+				INCREMENT BY 10
+				MAXVALUE 100
+				MINVALUE -100
+				CYCLE
+				CACHE 10;
+
+DROP SEQUENCE SEQ1;
+
+SELECT SEQ1.NEXTVAL FROM DUAL;
+SELECT SEQ1.CURRVAL FROM DUAL;
+
+ALTER SEQUENCE SEQ1
+		  INCREMENT BY 5
+		  MAXVALUE 250
+		  MINVALUE -100
+		  NOCYCLE 
+		  NOCACHE;
+		 
+CREATE TABLE TEMP(
+			ID NUMBER PRIMARY KEY 
+);
+
+CREATE SEQUENCE SEQ_TEMP;
+INSERT INTO TEMP VALUES(SEQ_TEMP.NEXTVAL);
+
+UPDATE TEMP 
+SET ID = SEQ_TEMP.NEXTVAL
+WHERE ID = 5;
+
+SELECT * FROM TEMP;
+
+/*
+ * NEXTVAL, CURRVAL 을 사용할 수 있는 명령어
+ * 		1. SELECT 문 (서브 쿼리 아님)
+ * 		2. INSERT 문의 VALUES 절
+ * 		3. INSERT 문의 SELECT 절
+ * 		4. UPDATE 문의 SET 절 
+ * 
+ * NEXTVAL, CURRVAL 을 사용할 수 없는 명령어 
+ * 		1. VIEW 를 사용하는 SELECT 
+ * 		2. DISTINCT 키워드가 있는 SELECT 
+ * 		3. GROUP BY, HAVING, ORDER BY 가 있는 SELECT
+ * 		4. SELECT, DELETE, UPDATE 의 서브쿼리 
+ * 		5. CREATE TABLE, ALTER TABLE 에서 사용하는 DEFAULT 값 D
+ */
